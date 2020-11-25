2b6fbce5814e90831ab00cf1c0b2d42d22ae8b20
cristiansavino <savinocristian89@gmail.com>
2020-11-25 09:24:35 +0000

    start to chatting

--- controllers/socketController.js
+++ controllers/socketController.js
@@ -1,5 +1,8 @@
 const User = require("../models/User");
 const Message = require("../models/Message");
+const {MongooseDeviceMessageRepository} = require("../src/DeviceMessages/Infrastucture/Repositories/MongooseDeviceMessageRepository");
+const {MongooseMessagePlayerRepository} = require("../src/MessagePlayers/Infrastucture/Repositories/MongooseMessagePlayerRepository");
+const {MongooseMessageRepository} = require("../src/Messages/Infrastucture/Repositories/MongooseMessageRepository");
 
 const usuarioConectado = async (uuid = "") => {
   const usuario = await User.findById(uuid);
@@ -28,6 +31,23 @@ const grabarMensaje = async (payload) => {
     }
   */
   try {
+
+    const messageRepository = new MongooseMessageRepository();
+    const messagePlayerRepository = new MongooseMessagePlayerRepository();
+    const deviceMessageRepository = new MongooseDeviceMessageRepository();
+
+    const newMessage = await messageRepository.createMessage(senderUser, messageData, chatRoom);
+
+    for (const user of users) {
+      const player = user.player;
+      const newMessagePlayer = await messagePlayerRepository.createMessagePlayer(senderUser, newMessage, player, chatRoom);
+
+      for (const device of user.devices) {
+
+        const newDeviceMessage = await deviceMessageRepository.createDeviceMessage(senderUser, newMessagePlayer, device, chatRoom, newMessage, player);
+
+      }
+    }
     const mensaje = new Message(payload);
     await mensaje.save();
     return mensaje;
--- sockets/socket.js
+++ sockets/socket.js
@@ -25,15 +25,42 @@ io.on("connection", (client) => {
   // ingresar al usuario a una sala especifica
   // sala global
   // sala uno a uno con el id del usuario
-  client.join(uuid);
+  // client.join(uuid);
+
+  client.on('enterChatRoom', async (enterChatRoomData) => {
+    console.log(client.rooms)
+  });
+
+  client.on('connectToChatRooms', async (user) => {
+    for (const chatRoom of user.chatRooms) {
+      client.join(chatRoom._id);
+    }
+  });
+
+  client.on('chatRoomMessage', async (payload) => {
+    client.broadcast.to(payload.chatRoom._id).emit('chatRoomMessage', {
+      'text': payload.text,
+      'de': payload.sender
+    })
+  });
+
 
   // escuchar del cliente mensaje-personal
-  client.on("mensaje-personal", async (payload) => {
-    // TODO grabar mensaje
-    await grabarMensaje(payload);
-
-    io.to(payload.para).emit("mensaje-personal", payload);
-  });
+  // client.on("mensaje-personal", async (payload) => {
+  //   console.log(payload.de._id)
+  //   for (const player of payload.para) {
+  //     console.log(player._id)
+  //     if (player._id !== payload.de._id) {
+  //       io.to(player._id).emit("mensaje-personal", payload);
+  //     }
+  //   }
+  //   // TODO grabar mensaje
+  //   // await grabarMensaje(payload);
+  //
+  //   // io.to(payload.de["_id"]).emit("mensaje-personal", payload);
+  //   //
+  //   // io.to(payload.para).emit("mensaje-personal", payload);
+  // });
 
   client.on("disconnect", () => {
     usuarioDesconectado(uuid);
